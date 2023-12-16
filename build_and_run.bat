e314581fbcb1da17d8567892e338db77fab6367d
Maxim <xalyes@gmail.com>
2023-12-16 17:59:51 +0200

    fix build_and_run.bat

--- build_and_run.bat
+++ build_and_run.bat
@@ -1,5 +1,5 @@
-cargo +nightly build -Zbuild-std --release --target x86_64-unknown-uefi^
- && cargo +nightly build -Zbuild-std=core --target kernel/x86_64-default_settings.json --release --manifest-path kernel/Cargo.toml^
- && xcopy /y target\x86_64-unknown-uefi\release\uefi_bootloader.efi build\EFI\BOOT\BOOTX64.EFI*^
- && xcopy /y target\x86_64-default_settings\release\kernel build\kernel*^
- && qemu-system-x86_64 -nodefaults -vga std -machine q35,accel=kvm:tcg -m 128M -drive "if=pflash,format=raw,readonly,file=build/OVMF_CODE.fd" -drive "if=pflash,format=raw,file=build/OVMF_VARS.fd" -drive "format=raw,file=fat:rw:build" -serial stdio -monitor vc:2560x1440 -no-reboot -no-shutdown
+cargo build -Zbuild-std=core --target x86_64-default_settings.json --release^
+ && xcopy /y target\x86_64-default_settings\release\rust_os build\kernel^
+ && cargo build --package loader --target x86_64-unknown-uefi -Z build-std=core -Z build-std-features=compiler-builtins-mem^
+ && cargo +stable run --package disk_image --target x86_64-pc-windows-msvc  -- target/x86_64-unknown-uefi/debug/loader.efi target/x86_64-default_settings/release/rust_os^
+ && qemu-system-x86_64 -drive format=raw,file=target/x86_64-unknown-uefi/debug/loader.gdt -bios build/OVMF_CODE.fd -monitor vc:2560x1440 -display gtk,zoom-to-fit=on -m 128M -device isa-debug-exit,iobase=0xf4,iosize=0x04 -serial stdio  -d int -D log.txt -no-reboot
