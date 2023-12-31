223566da9449fd72d0c86deffb1d40d19617db3a
Maxim <xalyes@gmail.com>
2023-12-31 11:46:58 +0200

    fix tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
-cargo build --package loader --target x86_64-unknown-uefi -Z build-std=core -Z build-std-features=compiler-builtins-mem &&
+cargo build --package loader --target x86_64-unknown-uefi -Z build-std-features=compiler-builtins-mem &&
 cargo test --release --workspace --exclude disk_image --exclude loader -- -display none
