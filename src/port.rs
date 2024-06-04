2d07ad9cf2cdabda4beed5ac4624ac517e79c292
Maxim <xalyes@gmail.com>
2024-06-05 00:23:14 +0300

    revert several files

--- src/port.rs
+++ src/port.rs
@@ -54,19 +54,3 @@ impl Port {
         value
     }
 }
-
-#[inline]
-pub unsafe fn write(port: u16, value: u8) {
-    unsafe {
-        asm!("out dx, al", in("dx") port, in("al") value, options(nomem, nostack, preserves_flags));
-    }
-}
-
-#[inline]
-pub unsafe fn read(port: u16) -> u8 {
-    let value: u8;
-    unsafe {
-        asm!("in al, dx", out("al") value, in("dx") port, options(nomem, nostack, preserves_flags));
-    }
-    value
-}
